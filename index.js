d1001417c1a36e3792304e6c3094863cfe80f24f
ishrak_imam <ishrak.imam@gmail.com>
2017-08-09 23:21:48 +0600

    cors fixed

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
   config = require('./config/main'),
-  router  = require('./router/router'),
-  cors = require('cors')
+  router = require('./router/router');
+
+// Enable CORS from client-side
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, ' +
+    'Accept, Authorization, Access-Control-Allow-Credentials')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  next()
+});
 
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 mongoose.connect(config.DATABASE, {
   server: {
@@ -17,15 +27,12 @@ mongoose.connect(config.DATABASE, {
       connectionTimeout: 0
     }
   }
-})
+});
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
-router(app)
+router(app);
 
-// Enable CORS from client-side
-app.use(cors())
-
-const server = app.listen(config.PORT)
-console.log(`Your server is running on port ${config.PORT}.`)
+const server = app.listen(config.PORT);
+console.log(`Your server is running on port ${config.PORT}.`);
